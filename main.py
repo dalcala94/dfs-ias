d034258ec4527dd691d00ab91f46853c50d51805
cminsung <38871509+cminsung@users.noreply.github.com>
2020-04-23 23:21:21 -0500

    Refined schedule maker by adding merging time ranges function.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import xlrd
 
 from datetime import timedelta
 
+from collections import defaultdict
+
 from instructor import Instructor
 from institution import Institution
 
@@ -30,10 +32,14 @@ def load_instructor_data(filepath):
 		Languages = sheet.cell_value(i,11)
 		ShirtSize = sheet.cell_value(i,12)
 		MultipleDays = sheet.cell_value(i,13)
-		Schedule = {(900,1020):list(build_list_int_days(sheet.cell_value(i,7))),
-					(930,1050):list(build_list_int_days(sheet.cell_value(i,8))),
-					(945,1065):list(build_list_int_days(sheet.cell_value(i,9)))}
 
+		Schedule = defaultdict(list)
+
+		make_schedule(Schedule, (900,1020), build_list_int_days(sheet.cell_value(i,7)))
+		make_schedule(Schedule, (930,1050), build_list_int_days(sheet.cell_value(i,8)))
+		make_schedule(Schedule, (945,1065), build_list_int_days(sheet.cell_value(i,9)))
+
+		print(Schedule)
 
 		instructors.append(Instructor(Name, Gender, Ethnicity, Region,
 			University, Year, PreviousMentor, Schedule,
@@ -68,6 +74,84 @@ def load_institution_data(filepath):
 
 	return institutions
 
+
+#TOOLS FOR MAKING SCHEDULE
+
+'''
+Represent schedule in a dictionary. 
+Keys are the days of the week in int. 
+Values are lists of time ranges.
+'''
+def make_schedule(schedule : dict, time_range : (int,int), 
+	days : [int]):
+	
+	for day in days:
+		schedule[day].append(time_range)
+
+
+def merge_time_range(current : (int,int), new : (int,int)) -> int:
+	
+	if current[0] <= new[0] <= current[1]:
+		if new[1] > current[1]:
+			return 1
+			#return (current[0], new[1]) 	#Have to remove current 
+		elif new[1] <= current[1]:
+			return 2
+	elif new[0] <= current[0] <= new[1]:
+		if current[1] > new[1]:
+			return 3
+			#return (new[0], current[1])		#Have to remove current
+		elif current[1] <= new[1]:
+			return 4
+			#No addition should be made
+	else:
+		return 5
+
+
+def modify_time_ranges(tlist : [(int,int)]) -> list:
+	new_tlist = list()
+
+	i = 0
+	j = i + 1
+	while tlist != []:
+		current = tlist[i]
+		compare = tlist[j]
+
+		merge = merge_time_range(current, compare)
+		if merge == 1:
+			new_time = (current[0], compare[1])
+			tlist.pop(0)
+			tlist.pop(0)
+			tlist.insert(0, new_time)
+		elif merge == 2:
+			tlist.pop(1)
+		elif merge == 3:
+			new_time = (compare[0], current[1])
+			tlist.pop(0)
+			tlist.pop(0)
+			tlist.insert(0, new_time)
+		elif merge == 4:
+			tlist.pop(0)
+		elif merge == 5:
+			j += 1
+
+		if (j >= len(tlist)):
+			j = i+1
+			new_tlist.append(tlist.pop(0))
+
+		if tlist == []:
+			break
+
+		if (i >= len(tlist)-1):
+			new_tlist.append(tlist.pop(0))
+
+	return new_tlist
+
+
+#TOOLS FOR MANIPULATING TIME
+
+
+
 '''
 Converts string time range in the format 15:00 - 17:00
 to a tuple of integer minutes (900, 1020)
@@ -89,40 +173,87 @@ def hours_to_minutes(time : str) -> int:
 	return result
 
 
+#TOOLS FOR MANIPULATING DAYS OF THE WEEK
+
+'''
+Convert days of a week to integers. 
+'''
 def days_to_int(day : str) -> int:
 	week = ["Sunday", "Monday", "Tuesday", "Wednesday",
 	"Thursday", "Friday", "Saturday"]
 
-	if day == "Sunday":
+	str_day = day.strip().lower()
+
+	if str_day == "sunday":
 		return 0
-	elif day == "Monday":
+	elif str_day == "monday":
 		return 1
-	elif day == "Tuesday":
+	elif str_day == "tuesday":
 		return 2
-	elif day == "Wednesday":
+	elif str_day == "wednesday":
 		return 3
-	elif day == "Thursday":
+	elif str_day == "thursday":
 		return 4
-	elif day == "Friday":
+	elif str_day == "friday":
 		return 5
-	elif day == "Saturday":
+	elif str_day == "saturday":
 		return 6
 	else:
 		return -1
 
+'''
+Build list of the days of the week in integer
+from a string of days. 
+'''
 def build_list_int_days(days : str) -> [int]:
 	str_list = list(days.split(","))
 
 	int_list = list()
 
 	for i in range(len(str_list)):
-		int_list.append(days_to_int(str_list[i].strip()))
+		int_day = days_to_int(str_list[i].strip())
+		if int_day != -1:
+			int_list.append(int_day)
 
 	return int_list
 
-instructors = load_instructor_data("shortinstructors.xlsx")	
+#TESTING
+
+
+#instructors = load_instructor_data("shortinstructors.xlsx")	
+'''
 institutions = load_institution_data("schoolsample.xlsx")
 
 for inst in instructors:
 	print(inst.name, inst.schedule)
+	break
+'''
 
+
+
+
+'''
+def modify_time_ranges(current : (int,int), schedule : dict, day : int) -> (int,int):
+
+	if schedule[day] == []:
+		schedule[day].append(current)
+
+	else:
+		print(schedule[day])
+		for time in schedule[day]:
+			new_time = merge_time_range(time, current)
+			if (new_time != time and new_time != current):
+				time = new_time
+				#print("1", time)
+			elif (new_time != time and new_time == current):
+				time = new_time
+				#print("2", time)
+			elif (new_time == time and new_time != current):
+				#print("3")
+				continue
+			else:
+				schedule[day].append(new_time)
+				#print("4", new_time)
+
+			#print(schedule[day])
+'''
