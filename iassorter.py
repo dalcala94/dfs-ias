83a2d1545cc9d01da5708a4453b853baae25f163
Daniel Alcala <dalcala94@gmail.com>
2020-04-30 17:13:33 -0700

    Update iassorter.py
    Update to function: randInstructToSchool()

--- iassorter.py
+++ iassorter.py
@@ -68,11 +68,36 @@ def print_result(result : dict):
 
 #Code heavily relies on Min and Appurva's initial matching algorithms...
 #function to randomely choose List.length() numer of times to assign/finalize matched instructors to a school in the region.
-#possible to teach multiple days (scheduled for x # of different schools on different days as pertaining to the instructors available data.
+#possible to teach multiple days (scheduled for x # of different schools on different days as pertaining to the instructors available data.)
 #@param passed in region dictionary with school objects as keys and a List() of matched instructors as values.
 #@return a dict with the proposed instructor assignment to matched school keys
-def randInstructToSchool(regionAndschools: dict)
-{
-    
-}
+def randInstructToSchool(regionAndSchools: dict) -> dict:
+
+    #Empty result dict. Result will be populated with school as keys and List() of instructors as values.
+     resultDict = {}
+
+     #Assuming List() associated with a school in this region are not the same...?
+     for key in regionAndSchools:
+             listLength = len(regionAndSchools[key])
+             print(listLength)
+             #Grab number of instrutors needed @ each school to perform rand alg. use key.instructors?
+             instructNeed = key.instructors
+             print("School " + key.name + " Needs: " + instructNeed + " Instructors!")
+
+
+###Testing randInstructToSchool() functionality. (Run in python shell)###
+#Reading input params seperately. (WORKS)
+#ocList = ["Jimmy", "Rick", "Eliza", "Beck", "Audrey", "Dan"]
+#ocReg = dict(Lathrop= ocList, Carr= ocList, Prentice= ocList)
+#print(ocReg)
+#randInstructToSchool(ocReg)
+
+#Reading keys as objects and values as lists. (WORKS)
+#dummyList = [Instructor("Daniel", "M", "M", "OC", "UCI", 2021, "N", "MWTh", "Y", "Eng", "L", "N"), Instructor("Julian", "M", "M", "OC", "UCI", 2021, "N", "MWTh", "Y", "Eng", "L", "N")]
+#key1 = Institution("Lathrop", "112 Apple", "App", 1, "MW")
+#key2 = Institution("Carr", "28 Bahia", "App", 1, "TTh")
+#key3 = Institution("Prentice", "242 Alton", "App", 1, "MF")
+#dummyDict = dict(key1= dummyList, key2= dummyList, key3= dummyList) #if printed, only lists object name keys and associated value address locations in memory.
+#randInstructToSchool(dummyDict) #Output = 2 2 2 ; Result = 2 2 2 
+
 
