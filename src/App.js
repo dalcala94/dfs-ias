b1c5c1ca86b90f6169c5cf59e0d23846d80ecdca
Tyler Yin <tyler.yin321@gmail.com>
2020-05-05 16:02:28 -0700

    firebase config file and router file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import fire from './fire';
+import UploadPage from './pages/upload.js';
+import DisplayPage from './pages/display.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+class App extends Component {
+  // constructor() {
+  //   super();
+  //   this.state = { 
+  //     username: '',
+  //     password: ''
+  //   }
+  //   this.handleChange = this.handleChange.bind(this);
+  //   this.handleSubmit = this.handleSubmit.bind(this);
+  // }
+
+  // handleChange(e) {
+  //   this.setState({
+  //     [e.target.name]: e.target.value
+  //   });
+  // }
+
+  // handleSubmit(e) {
+  //   e.preventDefault();
+  //   const usersRef = fire.database().ref('Users');
+  //   const user = {
+  //     user: this.state.username,
+  //     password: this.state.password
+  //   }
+  //   usersRef.push(user);
+  //   this.setState({
+  //     username: '',
+  //     password: ''
+  //   });
+  // }
+
+  // render() {
+  //   return (
+  //     <div className='app'>
+  //       <header>
+  //           <div className='wrapper'>
+  //             <h1>Adding Users</h1>
+              
+  //           </div>
+  //       </header>
+  //       <div className='container'>
+  //         <section className='add-user'>
+  //             <form onSubmit={this.handleSubmit}>
+  //               <input type="text" name="username" placeholder="Enter a username" onChange={this.handleChange} value={this.state.username} />
+  //               <input type="text" name="password" placeholder="Enter a password" onChange={this.handleChange} value={this.state.password} />
+  //               <button>Add User</button>
+  //             </form>
+  //         </section>
+  //         <section className='display-item'>
+  //           <div className='wrapper'>
+  //             <ul>
+  //             </ul>
+  //           </div>
+  //         </section>
+  //       </div>
+  //     </div>
+  //   );
+  // }
+
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Switch>
+            {/* UploadRosterPage Component */}
+            <Route path="/upload" component={UploadPage} />
+          </Switch>
+          <Switch>
+            {/*DisplayRosterPage Component */}
+            <Route path="/display" component={DisplayPage} />
+          </Switch>
+        </div>
+      </Router>
+    )
+  }
 }
 
 export default App;
